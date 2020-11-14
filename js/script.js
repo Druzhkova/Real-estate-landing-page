e144aa52bec6da4b32e7b403eff32203c7a628e1
Viktoriya <druzhkova.vika.v@gmail.com>
2020-11-14 22:09:28 +0300

    feat: add scroll to page

--- js/script.js
+++ js/script.js
@@ -10,3 +10,28 @@ stepBlocks.forEach((block, index) => {
     stepInfoLast[index].style.borderBottom = '3px solid black';
   })
 })
+
+// page scrolling
+
+const scrollButton = document.querySelector('.up-button');
+const upArrow = document.querySelector('.up-arrow');
+
+scrollButton.addEventListener('mouseover', () => {
+  upArrow.style.color = 'white';
+})
+
+scrollButton.addEventListener('mouseout', () => {
+  upArrow.style.color = 'black';
+})
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 200) {
+    scrollButton.classList.add('shown');
+  } else {
+    scrollButton.classList.remove('shown');
+  }
+})
+
+scrollButton.addEventListener('click', () => {
+  window.scrollTo(0, 0);
+})
