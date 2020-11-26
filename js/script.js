bc7a2d9536781be2214e5923ba6facf284f456f9
Viktoriya <druzhkova.vika.v@gmail.com>
2020-11-26 10:55:21 +0300

    feat: add apartment cost calculator

--- js/script.js
+++ js/script.js
@@ -1,37 +1,51 @@
-
-const stepBlocks = document.querySelectorAll('.stepblock')
-const stepInfoLast = document.querySelectorAll('.stepinfolast')
+const stepBlocks = document.querySelectorAll(".stepblock");
+const stepInfoLast = document.querySelectorAll(".stepinfolast");
 
 stepBlocks.forEach((block, index) => {
-  block.addEventListener('mouseover', () => {
-    stepInfoLast[index].style.borderBottom = '3px solid white';
-  })
-  block.addEventListener('mouseout', () => {
-    stepInfoLast[index].style.borderBottom = '3px solid black';
-  })
-})
+  block.addEventListener("mouseover", () => {
+    stepInfoLast[index].style.borderBottom = "3px solid white";
+  });
+  block.addEventListener("mouseout", () => {
+    stepInfoLast[index].style.borderBottom = "3px solid black";
+  });
+});
 
 // page scrolling
 
-const scrollButton = document.querySelector('.up-button');
-const upArrow = document.querySelector('.up-arrow');
+const scrollButton = document.querySelector(".up-button");
+const upArrow = document.querySelector(".up-arrow");
 
-scrollButton.addEventListener('mouseover', () => {
-  upArrow.style.color = 'white';
-})
+scrollButton.addEventListener("mouseover", () => {
+  upArrow.style.color = "white";
+});
 
-scrollButton.addEventListener('mouseout', () => {
-  upArrow.style.color = 'black';
-})
+scrollButton.addEventListener("mouseout", () => {
+  upArrow.style.color = "black";
+});
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   if (window.pageYOffset > 200) {
-    scrollButton.classList.add('shown');
+    scrollButton.classList.add("shown");
   } else {
-    scrollButton.classList.remove('shown');
+    scrollButton.classList.remove("shown");
   }
-})
+});
 
-scrollButton.addEventListener('click', () => {
+scrollButton.addEventListener("click", () => {
   window.scrollTo(0, 0);
-})
+});
+
+// calculator
+
+const calculator = document.querySelector(".calc");
+const buttonCalc = document.querySelector(".calculator");
+const closeCalc = document.querySelector(".close-calc");
+
+buttonCalc.addEventListener("click", function () {
+  calculator.classList.toggle("hidden");
+});
+
+closeCalc.addEventListener("click", function () {
+  calculator.classList.toggle("hidden");
+});
+
